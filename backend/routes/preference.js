5def42f9664d6fbc3fd29327fddb6eb02257cfd1
Young Yu <ybyu@Youngs-MacBook-Pro.local>
2022-10-21 23:46:25 -0400

    create HTTP GET request for getting user preferences

--- backend/routes/preference.js
+++ backend/routes/preference.js
@@ -13,16 +13,14 @@ router.post("/", async (req, res) => {
             const updatedPreferences = await Preference.findByIdAndUpdate(preferences._id, {
                 preferences: req.body.preferences
             });
-            res.status(201).json("Preferences were updated: " + updatedPreferences);
+            res.status(201).json("Preferences were updated.");
             return;
         } else {
             // save preferences to database
             const newPreferences = new Preference({
                 username: req.body.username,
                 preferences: req.body.preferences
-            }, {
-                new: true
-            });
+            }, { new: true });
             await newPreferences.save();
 
             res.status(201).json("New preferences were saved: " + newPreferences);
@@ -34,6 +32,27 @@ router.post("/", async (req, res) => {
     }
 });
 
-// TODO create GET call to get menu items
+// get preferences from database
+router.get("/", async (req, res) => {
+    try {
+        const preferences = await Preference.findOne({
+            username: req.body.username
+        });
+
+        if (!preferences) {
+            res.status(500).json("Error retrieving preferences (username likely doesn't exist)");
+            console.log("Error retrieving preferences (username likely doesn't exist)");
+            return;
+        }
+
+        res.status(200).json("Successfuly retrieved preferences: " + preferences);
+        console.log("Successfuly retrieved preferences: " + preferences);
+
+        return preferences;
+    } catch (error) {
+        res.status(500).json(error);
+        console.log("Error retrieving preferences: " + error);
+    }
+});
 
 module.exports = router;
