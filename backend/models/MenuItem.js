877433979bd364e4fb1a70ef77ac0adf74bf9d13
kim3470 <kim3470@purdue.edu>
2022-10-30 14:59:38 -0400

    replace menuInfo schema and add courtdata

--- backend/models/MenuItem.js
+++ backend/models/MenuItem.js
@@ -6,9 +6,7 @@ const MenuItemSchema = new mongoose.Schema( // create schema for Item in DB
     {
         ID: { type: String, required: true, unique: true },
         name: { type: String, required: true },
-        diningCourt: { type: [] }, //dining court that this menu item belongs to
-        station: { type: [] }, //station served at
-        mealType: { type: [] }, //breakfast, brunch, lunch, late lunch, or dinner
+        courtData: { type: [] }, //dining court, station, and meal type of this item       
         dateServed: { type: Date }, //date that this item was served (tentative field)
         isVegetarian: { type: Boolean, required: false },
         allergens: { type: [], required: false },
