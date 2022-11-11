dc00e3f15bd61a4bc4424a05373322a08897311b
kim3470 <70349283+kim3470@users.noreply.github.com>
2022-11-10 22:02:05 -0500

    Aaron temp (#53)
    * created loading logic for dining court information
    
    * created Dining Court schema model
    
    * removed old Locations model
    
    * implemented further logic for dining court parsing
    
    * commented in a method to get items matching a certain meal
    
    * updated dining court model to reflect array of objects
    
    * minor dining court changes
    
    * implemented loading dining court models in /api/load
    
    * implemented getting meals for each dining court
    
    * partially finished 34, 37, finished 10
    
    * finished story 10
    
    * sped up queries for meal times

--- backend/routes/menuInfo.js
+++ backend/routes/menuInfo.js
@@ -1,21 +1,110 @@
 const router = require("express").Router();
 const fetch = require('node-fetch');
 const MenuItem = require('../models/MenuItem');
+const DiningCourt = require('../models/DiningCourt')
 const Preference = require("../models/Preference");
 const Restriction = require("../models/Restriction");
 const User = require("../models/User");
 
-const DINING_COURTS = ["Wiley", "Earhart", "Ford", "Hillenbrand", "Windsor"];
+const DINING_COURTS = ["Earhart", "Ford", "Hillenbrand", "Wiley", "Windsor"];
 const PURDUE_DINING_URL = "https://dining.purdue.edu/menus/"; //unused
 const PURDUE_DINING_API_URL_MENU_ITEMS = "https://api.hfs.purdue.edu/menus/v2/items/";
 const PURDUE_DINING_API_URL_DINING_COURTS = "https://api.hfs.purdue.edu/menus/v2/locations/";
+const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 // LOAD - load menus site for current day
 router.post("/load", async (req, res) => { // use async/await to ensure request is fulfilled before writing to DB
     var d = new Date();
     var today = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
-    var todayDate = new Date(today);
+    var todayDay = d.getDay();
+    try { //parse outer xml
+        const response = await fetch(PURDUE_DINING_API_URL_DINING_COURTS);
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+        }
+        const json = await response.json(); //outer information stored in outer json
+        //debug: console.log(outerJson);
+        //iterate through locations listed in dining url
+        //structure: court -> day -> meal
+        for(var court of json.Location){
+            //if not a court in DINING_COURTS, skip
+            if (!(DINING_COURTS.find(courtname => (courtname === court.Name)))) {
+                continue
+            }
+            var name = court.Name
+            const formalName = court.FormalName
+            const placeID = String(court.GooglePlaceId)
+            var mealInfo = [] //[{meal name, start time, end time}]
+            //populate mealInfo array
+            for (var day of court.NormalHours[0].Days) {
+                //only select today's day, disregard other days
+                if (day.Name !== DAYS[todayDay]) {
+                    continue
+                }
+                //for each meal today calculate start and end times, convert to 12-hour format, and add suffix
+                for (var meal of day.Meals) {
+                    var type = meal.Name
+                    var start = meal.Hours.StartTime
+                    var end = meal.Hours.EndTime
+                    var sh = start.split(":")[0]
+                    var eh = end.split(":")[0]
+                    var sm = start.split(":")[1]
+                    var em = end.split(":")[1]
+                    var startSuffix = sh >= 12 ? " PM" : " AM"
+                    var endSuffix = eh >= 12 ? " PM" : " AM"
+                    var mealstart = ((sh % 12) || 12) + ":" + sm + startSuffix
+                    var mealend = ((eh % 12) || 12) + ":" + em + endSuffix
 
+                    var curmealinfo = {
+                        mealType: type,
+                        start: mealstart,
+                        end: mealend,
+                    }
+                    mealInfo.push(curmealinfo)
+                    //type contains final meal type
+                    //mealstart contains this meal's start time, mealend contains end time
+                }
+            }
+            //create object and push
+
+            //name contains court name
+            //formalName contains court's formal name
+            //mealInfo contains objects denoting meals start/end times
+            //googleID contains google place API key
+            try {
+                const diningCourtObj = await DiningCourt.findOne({
+                    name: name
+                });
+                if (diningCourtObj) { // if menu item already exists, update it with possibly new information
+                    await DiningCourt.findByIdAndUpdate(diningCourtObj._id, {
+                        name: name,
+                        formalName: formalName,
+                        googleID: placeID,
+                        mealInfo: mealInfo,
+                    });
+                    console.log("Updated dining court - " + name);
+                } else {// create new MenuItem for current menu item
+                    const newDiningCourt = new DiningCourt({
+                        name: name,
+                        formalName: formalName,
+                        googleID: placeID,
+                        mealInfo: mealInfo,
+                    });
+
+                    const newcourt = await newDiningCourt.save();
+                    // res.status(201).json(item); //return item in DB response to JSON
+                    console.log("Added dining court - " + name);
+                }
+            } catch (err) {
+                console.log("Error occured while parsing and saving dining court information");
+                console.log(err)
+            }
+        }
+    } catch (err) {
+        res.status(500).json(err);
+        console.log(err);
+    }
+    
     // Parse dining items for each dining court
     for (const diningCourt of DINING_COURTS) {
         const outerUrl = PURDUE_DINING_API_URL_DINING_COURTS + diningCourt + "/" + today;
@@ -39,30 +128,15 @@ router.post("/load", async (req, res) => { // use async/await to ensure request 
                         if (!response.ok) {
                             throw new Error(`Error! status: ${response.status}`)
                         }
-
                         const json = await response.json();
-
                         const courtdata = [diningCourt, stationname, type]
-
                         try {
                             const menuItem = await MenuItem.findOne({
                                 ID: json.ID
                             });
-
-                            /* if menu item date is different, reset court data and update info */
-                            if (menuItem && menuItem.dateServed.getDate != todayDate.getDate) {
-                                await MenuItem.findByIdAndUpdate(menuItem._id, {
-                                    ID: json.ID,
-                                    name: json.Name,
-                                    courtData: [courtdata],
-                                    dateServed: today,
-                                    isVegetarian: json.IsVegetarian,
-                                    allergens: json.Allergens,
-                                    nutritionFacts: json.Nutrition,
-                                    ingredients: json.Ingredients
-                                })
-                            } else if (menuItem) { // if menu item already exists, update it with possibly new information
-                                await MenuItem.findByIdAndUpdate(menuItem._id, { $addToSet: { courtData: courtdata } }, { /* use addToSet to prevent duplicates */
+                            //item.dateServed.getDate() === today.getDate()
+                            if (menuItem && menuItem.dateServed.getDate() === d.getDate()) { //if menu item exists and was served today, update it with possibly new information 
+                                await MenuItem.findByIdAndUpdate(menuItem._id, { $addToSet: { courtData: courtdata } }, {
                                     ID: json.ID,
                                     name: json.Name,
                                     dateServed: today,
@@ -72,7 +146,19 @@ router.post("/load", async (req, res) => { // use async/await to ensure request 
                                     ingredients: json.Ingredients
                                 });
                                 console.log("Updated menu item - " + diningCourt + ": " + json.Name);
-                            } else { // create new MenuItem for current menu item
+                            } else if(menuItem) { //if menu item exists and is for a different day, replace courtdata & update info
+                                await MenuItem.findByIdAndUpdate(menuItem._id, {
+                                    ID: json.ID,
+                                    name: json.Name,
+                                    dateServed: today,
+                                    courtData: courtdata,
+                                    isVegetarian: json.IsVegetarian,
+                                    allergens: json.Allergens,
+                                    nutritionFacts: json.Nutrition,
+                                    ingredients: json.Ingredients
+                                });
+                                console.log("Updated menu item (reset courtdata) - " + diningCourt + ": " + json.Name);
+                            } else {// create new MenuItem for current menu item
                                 const newMenuItem = new MenuItem({
                                     ID: json.ID,
                                     name: json.Name,
@@ -95,15 +181,14 @@ router.post("/load", async (req, res) => { // use async/await to ensure request 
                         }
                     }
                 }
-
             }
         } catch (err) {
             res.status(500).json(err);
             console.log(err);
         }
     }
-    res.status(201).json("Dining data was parsed successfully for " + today);
-    console.log("Dining data was parsed successfully for " + today);
+    res.status(201).json("Dining/dining courts data was parsed successfully for " + today);
+    console.log("Dining/dining courts data was parsed successfully for " + today);
 });
 
 
@@ -259,7 +344,6 @@ router.post("/prefsAndRests/:diningCourt", async (req, res) => {
                         skipPrefs = true;
 
                     }
-
                 });
                 if (!skipPrefs) matchingItems.push(item); // this item matches both prefs & rests
             }
@@ -269,7 +353,6 @@ router.post("/prefsAndRests/:diningCourt", async (req, res) => {
         res.status(500).json(error);
         console.log(error);
     }
-
 });
 
 // this endpoint returns all menu items of the provided dining court
@@ -281,11 +364,7 @@ router.get("/:diningCourt", async (req, res) => {
         const menuItems = await MenuItem.find({
             dateServed: today,
             courtData: {
-                $elemMatch: {
-                    $elemMatch: {
-                        $in: [req.params.diningCourt]
-                    }
-                }
+                $elemMatch: { $elemMatch: { $in: [req.params.diningCourt] } }
             }
         });
 
@@ -293,36 +372,95 @@ router.get("/:diningCourt", async (req, res) => {
             res.status(500).json("No items found");
             return;
         }
-
         res.status(200).json(menuItems);
     } catch (error) { console.log(error); }
-
 });
+
+//this endpoint returns all menu items of provided dining court that are serving during the meal specified
+router.get("/meals/:diningCourt/:meal", async (req, res) => {
+    //debug:
+    var d = new Date();
+    var today = new Date(d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate());
+    try {
+        const meal = req.params.meal.replace("-", " ")
+        const menuItems = await (MenuItem.find({
+            $and: [{
+                dateServed: today,
+                courtData: {
+                    $elemMatch: { $elemMatch: { $in: [req.params.diningCourt] } }
+                }
+            },
+            {
+                dateServed: today,
+                courtData: {
+                    $elemMatch: { $elemMatch: { $in: [req.params.meal] } }
+                }
+            }]
+        }))
+        if (!menuItems) { //this means items were not found
+            res.status(500).json("No items found");
+            return;
+        }
+
+        // let matches = [];
+        // menuItems.forEach((item) => {
+        //     let courtData = item.courtData;
+        //     let visited = false;
+
+        //     if (courtData === null) return;
+
+        //     //iterate through court data array and find items that match parameters
+        //     courtData.forEach((court) => {
+        //         if (!visited && court.includes(req.params.diningCourt) && court.includes(meal) && item.dateServed.getTime() === today.getTime()) {
+        //             matches.push(item);
+        //             visited = true;
+        //         }
+        //     });
+        // });
+        console.log("Successfully retrieved " + req.params.diningCourt + "'s " + req.params.meal + " menu")
+        res.status(200).json(menuItems);
+    } catch (error) { console.log(error); }
+});
+
+//this endpoint returns the specified court's information
+router.get("/courts/:diningCourt", async (req,res) => {
+    try {
+        const diningCourt = req.params.diningCourt;
+        const court = await DiningCourt.findOne({
+            name: diningCourt
+        });
+        if (court == null) {
+            res.status(500).json("No court found");
+            return;
+        }
+        res.status(200).json(court);
+        return;
+    } catch (error) {
+        res.status(500).json("Error: " + error);
+        console.log("Error: " + error);
+    }
+})
 
 // this endpoint returns all menu items of the provided dining court that aligns 
 // with a user's dietary preferences and restrictions
 router.get("/prefs/:diningCourt/:username", async (req, res) => {
     var d = new Date();
     var today = new Date(d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate());
-
     // console.log("today is " + d);
     try {
         const user = await User.findOne({
             username: req.params.username
         });
-
         if (!user) {
             res.status(500).json("User doesn't exist");
             return;
         }
-
         const prefResponse = await Preference.findOne({
             username: req.params.username
         });
         const restResponse = await Restriction.findOne({
             username: req.params.username
         });
-
         // get preferences from response
         let preferences;
         if (!prefResponse) {
@@ -331,7 +469,6 @@ router.get("/prefs/:diningCourt/:username", async (req, res) => {
         else {
             preferences = prefResponse.preferences;
         }
-
         // get restrictions from response
         let restrictions;
         if (!restResponse) {
@@ -357,9 +494,7 @@ router.get("/prefs/:diningCourt/:username", async (req, res) => {
             res.status(200).json([]); // empty
             return;
         }
-
         let courtsItems = [];
-
         menuItems.forEach((item) => {
             let courtsArray = item.courtData;
 
@@ -397,7 +532,6 @@ router.get("/prefs/:diningCourt/:username", async (req, res) => {
                 courtsItems.push(item);
             }
         });
-
         res.status(200).json(courtsItems);
     } catch (error) { 
         console.log(error); 
@@ -405,20 +539,16 @@ router.get("/prefs/:diningCourt/:username", async (req, res) => {
 });
 
 /*
-
 Returns a menu item based on the provided item ID
 
 Example Call: http://localhost:8000/api/menuInfo/item/76f9d158-d45d-42e0-8e37-8bd3c2c45986
 Returns this object: 
 {
-
     ...
     "ID": "76f9d158-d45d-42e0-8e37-8bd3c2c45986",
     "name": "Scrambled Eggs With Bacon And Cheese"
     ...
-
 }
-
 */
 router.get("/item/:menuItemID", async (req, res) => {
 
@@ -434,17 +564,15 @@ router.get("/item/:menuItemID", async (req, res) => {
             res.status(500).json("No item found");
             return;
         }
-
         res.status(200).json(item);
         return;
+    //} catch (error) {
 
     } catch (error) {
 
         res.status(500).json("Error: " + error);
         console.log("Error: " + error);
-
     }
-
 });
 
 module.exports = router;
