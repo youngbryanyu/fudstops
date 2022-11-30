9888898ea9ab4c14f399b359ea89a95de3374097
vsahani007 <41131544+vsahani007@users.noreply.github.com>
2022-11-30 16:41:24 -0500

    finished sorting favorites (#58)

--- frontend/src/pages/favorites/Favorites.jsx
+++ frontend/src/pages/favorites/Favorites.jsx
@@ -6,13 +6,18 @@ import Box from "@material-ui/core/Box";
 import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext, useRef } from 'react';
 import { AuthContext } from "../../authContext/AuthContext";
 import axios from "axios";
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 
 const Favorites = () => {
 
     const [courtsMenu, setCourtsMenu] = useState([]); //the current items displayed in list
+    const [menuBeforeSort, setMenuBeforeSort] = useState([]); // items displayed before sorting (courtsmenu)
+    const [shouldSort, setShouldSort] = useState(false);
     const { user } = useContext(AuthContext);
     const username = user.username;
 
@@ -29,6 +34,12 @@ const Favorites = () => {
         );
     }
 
+      /* sorting */
+      const handleSortClick = () => {
+        setShouldSort(!shouldSort);
+    }
+
+   
     /**
     * Load dining courts items on page load and alters anytime the location changes
     */
@@ -52,6 +63,31 @@ const Favorites = () => {
         // eslint-disable-next-line
     }, []);
 
+    //console.log(courtsMenu.length);
+
+     /* Sorting useEffect */
+     const isFirstRender = useRef(true);
+     useEffect(() => {
+         if (isFirstRender.current === true) {
+             isFirstRender.current = false;
+             return;
+         }
+         // sort courts menu then set it to the sorted
+         if (shouldSort) {
+             //this does a copy of the prior menu
+             setMenuBeforeSort(JSON.parse(JSON.stringify(courtsMenu))); //unsorted items now stored in menuBeforeSort
+             //now we sort the item (this is an inline function that compares two objects names)
+             //this means (if a's name > b's name) then return 1
+             //            else return (if b's name > a's name) then 1 
+             //                         else return 0 which means both names are equal
+             courtsMenu.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+         } else if(!shouldSort) {
+             //set courtsMenu back to the way it was originally
+             setCourtsMenu(menuBeforeSort);
+         }
+         // eslint-disable-next-line
+     }, [shouldSort]);
+
     return (
         <div className="menu">
             <Navbar />
@@ -64,6 +100,9 @@ const Favorites = () => {
                         </List>
                     </Paper>
                 </Box>
+                <FormGroup>
+                        <FormControlLabel control={<Checkbox size="small" color="secondary" />} label={"Sort Alphabetically"} checked={shouldSort} onChange={handleSortClick}/>
+                    </FormGroup>
             </div>
         </div>
     );
